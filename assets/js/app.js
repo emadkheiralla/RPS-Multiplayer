c318b6979663bba6e975af74fe88f39f59f1afae
Emad Kheiralla <Emad Kheiralla>
2016-06-28 13:55:21 -0400

    updated logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -144,68 +144,57 @@ firebase.initializeApp(config);
 
 var dbRef = firebase.database().ref();
 var users = 0;
-var p1pick = "";
-var p1wins = 0;
-var p1losses = 0;
-var p1ties = 0;
-var p2pick = "";
-var p2wins = 0;
-var p2losses = 0;
-var p2ties = 0;
 
-var Player1 = {
-	Name: "",
-	Pick: p1pick,
-	Registered: false,
-	CurrentPlayer: false,
-	Wins: p1wins,
-	Losses: p1losses,
-	Ties: p1ties
-};
-
-var Player2 = {
-	Name: "",
-	Pick: p2pick,
-	Registered: false,
-	CurrentPlayer: false,
-	Wins: p2wins,
-	Losses: p2losses,
-	Ties: p2ties
-};
-
+dbRef.child("Users").set(users);
 
 
 $('#playerSubmit').on('click', function () {
-	var name = $('.username').val();	
+	var name = $('.username').val();
+	
+	var p1pick = "";
+	var p1wins = 0;
+	var p1losses = 0;
+	var p1ties = 0;
+	var p2pick = "";
+	var p2wins = 0;
+	var p2losses = 0;
+	var p2ties = 0;
+
+	var Player1 = {
+		Name: "",
+		Pick: p1pick,
+		Registered: false,
+		CurrentPlayer: false,
+		Wins: p1wins,
+		Losses: p1losses,
+		Ties: p1ties
+	};
+
+	var Player2 = {
+		Name: "",
+		Pick: p2pick,
+		Registered: false,
+		CurrentPlayer: false,
+		Wins: p2wins,
+		Losses: p2losses,
+		Ties: p2ties
+	};
+
     if(users == 0){
-    	console.log(users);
 		Player1.Registered = true;
 		Player1.CurrentPlayer = true;
-		Player1.Name = name;
-		dbRef.child("Users").set(users);
-		users++;
-		dbRef.child("Player1").set(Player1);
-		dbRef.Player1.Name.set(name);
-		dbRef.Player1.Pick.set(p1pick);
-		dbRef.Player1.Wins.set(p1wins);
-		dbRef.Player1.Losses.set(p1losses);
-		dbRef.Player1.Ties.set(p1ties);		
+		Player1.Name = name;		
+		dbRef.child("Player1").set(Player1);		
 		clearForm();
+		users++;
 	}else if(users == 1){
-		console.log(users);
 		Player2.Registered = true;
 		Player2.CurrentPlayer = true;
-		Player2.Name = name;
-		users++;
+		Player2.Name = name;		
 		dbRef.child("Player2").set(Player2);
-		dbRef.Player2.Name.set(name);
-		dbRef.Player2.Pick.set(p2pick);
-		dbRef.Player2.Wins.set(p2wins);
-		dbRef.Player2.Losses.set(p2losses);
-		dbRef.Player2.Ties.set(p2ties);
 		clearForm();
-		//fight();
-		
+		users++;
+		fight();		
 	}else if(users >=2){
 		console.log("There are already 2 players registered!");		
 	}
@@ -214,8 +203,9 @@ $('#playerSubmit').on('click', function () {
 
 dbRef.on('value', function (snapshot) {
 	console.log(snapshot.val());
+
+	var currusers = snapshot.val().Users;
 	console.log(currusers);
-	var currusers = snapshot.child("Users").val();
 	if(currusers == 0){
 		console.log("No Users Online");
 		currusers++;
